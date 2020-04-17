9f27fd67baf6f2fd9c9caf5b23c48d83628c061b
Frank Karunaratna <frankdilank@hotmail.com>
2020-04-16 21:16:28 -0400

    Run unit tests on travis ci

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
 
 export const connectDb = () => {
+  // Travis CI database for testing
+  if (process.env.TRAVIS === "true") {
+    return mongoose.connect("mongodb://localhost:27017/myapp", {
+      useNewUrlParser: true,
+    });
+  }
+
   return mongoose.connect(process.env.DB_URI, {
     poolSize: 10,
     useNewUrlParser: true,
