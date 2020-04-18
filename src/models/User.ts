88ec23c183b48a1c1c439f9583a49a890c7a3795
Frank Karunaratna <frankdilank@hotmail.com>
2020-04-18 17:00:43 -0400

    Clean up User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,6 @@
 import * as mongoose from "mongoose";
 import IUser from "../interfaces/IUser";
 
-export interface ITestModel extends mongoose.Model<IUser> {
-  // here we decalre statics
-
-  findByUsername(username: string): Promise<IUser>;
-}
-
 const userSchema: mongoose.Schema = new mongoose.Schema(
   {
     username: {
@@ -22,14 +16,6 @@ const userSchema: mongoose.Schema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-
-userSchema.statics.findByUsername = async function (username: string) {
-  const user: IUser = await this.findOne({
-    username,
-  });
-
-  return user;
-};
 
 const userModel: mongoose.Model<IUser> = mongoose.model<IUser>(
   "User",
